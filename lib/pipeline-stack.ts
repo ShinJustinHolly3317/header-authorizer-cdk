c827c5d85f3f05e32e6a0185b15e0d53ac87cbdc
Justin Kao <justin.kao@choco.media>
2022-07-10 20:41:45 +0800

    add codepipeline construct

--- lib/pipeline-stack.ts
+++ lib/pipeline-stack.ts
@@ -1,14 +1,30 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as codecommit from 'aws-cdk-lib/aws-codecommit';
-
+import {CodeBuildStep, CodePipeline, CodePipelineSource} from "aws-cdk-lib/pipelines";
 
 export class WorkshopPipelineStack extends cdk.Stack {
     constructor(scope: Construct, id: string, props?: cdk.StackProps) {
         super(scope, id, props);
 
-        new codecommit.Repository(this, 'BasicAuthHandler', {
+        const repo = new codecommit.Repository(this, 'BasicAuthHandler', {
             repositoryName: "BasicAuthHandler"
         });
+
+        const pipeline = new CodePipeline(this, 'Pipeline', {
+          pipelineName: 'WorkshopPipeline',
+          synth: new CodeBuildStep('SynthStep', {
+                  input: CodePipelineSource.codeCommit(repo, 'master'),
+                  installCommands: [
+                      'npm install -g aws-cdk'
+                  ],
+                  commands: [
+                      'npm ci',
+                      'npm run build',
+                      'npx cdk synth'
+                  ]
+              }
+          )
+      });
     }
 }
