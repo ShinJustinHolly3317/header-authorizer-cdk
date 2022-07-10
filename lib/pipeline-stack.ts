d4e77df0d328440456a49be9baadf1ca2cac77a8
Justin Kao <justin.kao@choco.media>
2022-07-10 22:28:29 +0800

    Add deploy stage to pipeline

--- lib/pipeline-stack.ts
+++ lib/pipeline-stack.ts
@@ -2,6 +2,7 @@ import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as codecommit from 'aws-cdk-lib/aws-codecommit';
 import {CodeBuildStep, CodePipeline, CodePipelineSource} from "aws-cdk-lib/pipelines";
+import { BasicAuthPipelineStage } from './pipeline-stage'
 
 export class WorkshopPipelineStack extends cdk.Stack {
     constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -29,5 +30,8 @@ export class WorkshopPipelineStack extends cdk.Stack {
               }
           )
       });
+
+      const deploy = new BasicAuthPipelineStage(this, 'Deploy')
+      const deployStage = pipeline.addStage(deploy)
     }
 }
